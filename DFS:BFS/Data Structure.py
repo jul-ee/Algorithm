7e3580a18d530b8ab9d8fbbd294abf8f34502279
jul-ee <160218278+jul-ee@users.noreply.github.com>
2024-05-01 22:23:54 +0900

    DataStructure_Queue

--- DFS:BFS/Data Structure.py
+++ DFS:BFS/Data Structure.py
@@ -12,3 +12,21 @@ stack.pop()
 
 print(stack)  # 최하단 원소부터 출력
 print(stack[::-1])  # 최상단 원소부터 출력
+
+# Queue
+from collections import deque
+
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue)
+queue.reverse()
+print(queue)
